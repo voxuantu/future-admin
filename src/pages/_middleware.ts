4ed7cb25adf7c3121a9d5a5cd639cd78ca072e56
voxuantu <19522461@gm.uit.edu.vn>
2023-06-08 22:32:58 +0700

    feat: protect route

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -3,16 +3,32 @@ import { NextResponse } from 'next/server'
 import adminApi from '../api/admin-api'
 
 export async function middleware(req: NextRequest, event: NextFetchEvent) {
-  console.log('req.nextUrl.basePath: ', req.nextUrl.pathname)
   if (req.nextUrl.pathname === '/login/') {
     return NextResponse.next()
   }
 
-  try {
-    await adminApi.validate()
-
+  const token = req.cookies['Authorization']
+  console.log('token: ', token)
+  const data = await fetch(`http://localhost:5500/api/v1/admin/validate`, {
+    headers: {
+      Authorization: `Bearer ${token}`
+    }
+  })
+  // .then((data: any) => {
+  //   console.log('ata.status: ', data.status)
+  //   if (data.status === 200 || data.status == 201) {
+  //     return NextResponse.next()
+  //   } else {
+  //     return NextResponse.redirect('/login') // redirect
+  //   }
+  // })
+  // .catch(() => {
+  //   return NextResponse.redirect('/login') // redirect
+  // })
+  console.log('data.status: ', data.status)
+  if (data.status === 200 || data.status == 201) {
     return NextResponse.next()
-  } catch (error) {
+  } else {
     return NextResponse.redirect('/login') // redirect
   }
 }
